3fad13bc821cc699ca477fbffa2e7bcd012fc178
Agus Priyatin <aguspriyatin80@gmail.com>
2022-08-17 13:42:37 +0700

    create interface UserService, class UserServiceImpl -> createUser

---
+++ src/main/java/com/miniproject/blog/services/UserService.java
@@ -0,0 +1,8 @@
+package com.miniproject.blog.services;
+
+import com.miniproject.blog.payloads.UserDTO;
+
+public interface UserService {
+
+	UserDTO createUser(UserDTO user);
+}
---
+++ src/main/java/com/miniproject/blog/services/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.miniproject.blog.services.impl;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.miniproject.blog.entities.User;
+import com.miniproject.blog.payloads.UserDTO;
+import com.miniproject.blog.repositories.UserRepo;
+import com.miniproject.blog.services.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepo userRepo;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	@Override
+	public UserDTO createUser(UserDTO userDTO) {
+		User user = this.modelMapper.map(userDTO, User.class);
+		User addedUser = this.userRepo.save(user);
+		return this.modelMapper.map(addedUser, UserDTO.class);
+	}
+	
+	
+
+}
