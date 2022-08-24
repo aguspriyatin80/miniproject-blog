ab062112a6e1a27c0083e51150a65bdfae3193b7
Agus Priyatin <aguspriyatin80@gmail.com>
2022-08-24 09:55:40 +0700

    delete enum

--- src/main/java/com/miniproject/blog/repositories/UserResponseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.miniproject.blog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.miniproject.blog.entities.UserRegister;
-
-public interface UserResponseRepo extends JpaRepository<UserRegister,Integer> {
-
-}
