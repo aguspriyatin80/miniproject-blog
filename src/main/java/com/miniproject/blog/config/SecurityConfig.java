5e01ca81817f85ea739ae5ab3dd1c52162bd6b6a
Agus Priyatin <aguspriyatin80@gmail.com>
2022-08-25 21:53:15 +0700

    implement Spring Security without using the WebSecurityConfigurerAdapter

--- src/main/java/com/miniproject/blog/config/SecurityConfig.java
+++ src/main/java/com/miniproject/blog/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.miniproject.blog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +18,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -24,7 +31,7 @@ import com.miniproject.blog.security.JwtAuthenticationFilter;
 @EnableWebSecurity
 @EnableWebMvc
 @EnableGlobalMethodSecurity(prePostEnabled=true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig{
 
 	public static final String[] PUBLIC_URLS = {
 			"/api/v1/auth/**",
@@ -43,8 +50,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
 		http
 		.csrf()
 		.disable()
@@ -60,12 +68,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		
+		
+		http.authenticationProvider(daoAuthenticationProvider());
+		DefaultSecurityFilterChain defaultSecurityFilterChain = http.build();
+		
+		return defaultSecurityFilterChain;
 	}
+	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http
+//		.csrf()
+//		.disable()
+//		.authorizeHttpRequests()
+//		.antMatchers(PUBLIC_URLS).permitAll()
+//		.antMatchers(HttpMethod.GET).permitAll()
+//		.anyRequest()
+//		.authenticated()
+//		.and()
+//		.exceptionHandling().authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
+//		.and()
+//		.sessionManagement()
+//		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//		
+//		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+//	}
+	
+	
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(this.customUserDetailService).passwordEncoder(passwordEncoder());
-	}
+	
+	
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.userDetailsService(this.customUserDetailService).passwordEncoder(passwordEncoder());
+//	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder()
@@ -73,12 +110,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
+//	@Bean
+//	public AuthenticationManager authenticationManagerBean() throws Exception {
+//		return super.authenticationManagerBean();
+//	}
+	
+	
 	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		// TODO Auto-generated method stub
-		return super.authenticationManagerBean();
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(this.customUserDetailService);
+		provider.setPasswordEncoder(passwordEncoder());
+		return provider;
 	}
 
+	
+	@Bean
+	public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration ) throws Exception {
+		return configuration.getAuthenticationManager();
+	}
+	
 	
 }
